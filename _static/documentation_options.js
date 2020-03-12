3ddec9b43abf1a339ccea3fc785f2dbed69fdb6a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-12 18:22:59 +0000

    Deploy brainhack-princeton/handbook to github.com/brainhack-princeton/handbook.git:gh-pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.1.0+3.gdf73fdd.dirty',
+    VERSION: 'v0.1.0+5.ga9ab611.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
